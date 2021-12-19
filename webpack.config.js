710ddfa1c6e8c07c448eec0836226cefa060cc68
unknown <vikasa.agarwal@gmail.com>
2021-12-19 12:35:36 +0530

    added scss css loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,19 @@ module.exports = {
     devtool: false,
     entry: "./src/index_src.js",
     output: {
-        filename: "main.js",
+        filename: "main.[contenthash].js",
         path: path.resolve(__dirname, 'dist')
+    },
+    "module": {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    "sass-loader"
+                ]
+            }
+        ]
     }
 };
